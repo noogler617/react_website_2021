79e1f8b153b94e8c2f58089eceff17beb61f868b
noogler617 <themolenator@gmail.com>
2021-10-12 12:45:33 -0400

    update home

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Astro from '../astro2.jpg'
+import Astro from '../Astro-Farm.jpg'
 import './home.css'
 
 
@@ -10,7 +10,7 @@ export class Home extends Component {
           <div id="image">
             <img src={Astro} alt="Stars in the sky" />
 
-                <h1>Welcome to Ryan&rsquo;s website.</h1>
+                <h1>Welcome to my website. Have a look around.</h1>
 
 
           </div>
