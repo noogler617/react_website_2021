2ddc4e8bfdb8624ea8f769bda774af745c203da8
noogler617 <themolenator@gmail.com>
2021-10-12 07:05:56 -0400

    update about page

--- src/components/about.js
+++ src/components/about.js
@@ -16,7 +16,10 @@ export class About extends Component {
           <p>Hello, </p>
           <p>My name is Ryan, welcome to my website.</p>
           <p>I currently work in IT as a system administrator.
-           I enjoy working with networks, firewalls, Cloud (Azure, AWS) services, creating and breaking Linux servers, and coding with Python.</p>
+           I enjoy working with networks, firewalls, Cloud (Azure, AWS) services, creating and breaking Linux servers, and practice coding with Python.</p>
+
+           <p>When not on a computer I enjoy writing and playing music.
+            I have been playing music for over 20 years and played many of Boston's music venues such as The Paradise Rock Club, Harper's Ferry, the Middle East.</p>
           </div>
 
         </Container>
